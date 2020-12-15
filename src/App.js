c51f92b5246ac8a3b3d207381447c21284802082
Syed Ahmed <syed-hamdan@hotmail.com>
2020-12-15 17:38:29 -0500

    Added functioning Visualize, Stop Visualize, and Regenerate Array buttons, and implemented bubblesort algorithm

--- src/App.js
+++ src/App.js
@@ -1,46 +1,128 @@
 import React from 'react';
 import TopBar from './components/TopBar';
-import Bubble from './components/Bubble';
+import p5 from 'p5';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
 
 
 class App extends React.Component {
   // nodes stores line information, algorithm stores type of algorithm selected from dropdown 
-  state = {
-    nodes: [],
-    algorithm : "0"
-  };
+  constructor() {
+    super();
 
-  updateParent = val => {
-    this.setState({
-      algorithm : val
-    }, () => console.log(this.state.algorithm) );
+    this.state = {
+      nodes : [],
+      numNodes: 1600,
+      algorithm : "0"
+    };
+
+    this.renderRef = React.createRef();
+    this.continueSorting = true;
+
+    // p5 sketch
+    this.data = new p5(p => {    
+        p.setup = () => {
+            p.createCanvas(1600,560).parent(this.renderRef.current);
+            
+            // assign random values to nodes array for data lines
+            for (let i = 0; i < this.state.numNodes; i++) {
+              this.state.nodes[i] = (  (Math.floor(Math.random() * 1000) )  );
+            }
+              
+        };
+    
+        p.draw = () => {
+            let lineHeight;
+            p.background(255);
+
+            for (let i = 0; i < this.state.numNodes; i++) {
+              lineHeight = this.state.nodes[i];
+              p.stroke(0);
+              p.line(i, window.screen.height, i, window.screen.height - lineHeight);
+            }
+        };
+    });
+
+
+
+    this.swap = this.swap.bind(this);
+    this.bubbleSort = this.bubbleSort.bind(this);
+  }
+
+  
+  async sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 
-  render() {
-    let algoComponent;
-    switch (this.state.algorithm) {
-      case '1': 
-        algoComponent = <Bubble />;
-        break;
-      // // case 2: 
-      //   algoComponent = <Merge/>;
-      //   break;
-      // // case 3: 
-      //   algoComponent = <Quick />;
-      //   break;
-      default: 
-        algoComponent = 0;
-        break;
+  // BUBBLE SORT FUNCTIONS //
+
+  async swap(array, a, b) {
+    await this.sleep(1);
+    let tempVal;
+
+    tempVal = array[a];
+    array[a] = array[b];
+    array[b] = tempVal;
+
+    this.setState({
+      nodes: array
+    });
+  }
+
+  async bubbleSort() {
+    this.continueSorting = true;
+    let data = [...this.state.nodes];
+    let dataLength = data.length - 1;
+    let isSorted = false;
+  
+    while(!isSorted) {
+      isSorted = true;
+
+      while (this.continueSorting) {
+        for (let i = 0; i < dataLength; i++) {
+          if (data[i] > data[i + 1]) {
+            await this.swap(data, i, i + 1);
+            isSorted = false;
+          }
+        }
+      }
+      dataLength--; 
     }
+  }
+
+  // stops sorting function
+  stopSort = () => {
+    this.continueSorting = false;
+  }
+
+  // testing function
+  equal(array1, array2) {
+    if (array1.length !== array2.length) return false;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    } 
+
+    console.log(array1);
+    console.log(array2);
+    return true;
+
+    // let data1 = this.state.nodes.slice().sort( (a, b) => a - b);
+  } 
+
+
+
+  render() {
 
     return (
       <React.Fragment>
-        <TopBar algorithm = {this.state.algorithm} updateParent = {this.updateParent}/>
-        {algoComponent}
-      </React.Fragment>
+        <TopBar algorithm = {this.state.algorithm} updateParentAlgo = {this.updateParentAlgo} regen = {this.data.setup} bubbleSort = {this.bubbleSort} stopSort = {this.stopSort}/>
+        <div className = "d-flex justify-content-center" id="renderWrapper">
+          <div id="renderDiv" ref={this.renderRef}></div>
+        </div>
+      </React.Fragment> 
     );
   }
 
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 
+
 class TopBar extends React.Component {
     constructor(props) {
         super(props);
@@ -9,28 +10,41 @@ class TopBar extends React.Component {
         this.myRef = React.createRef();
     }
     
-    // handles form submission by using updateParent() to change Parent component state
+    // // handles form submission by using updateParent() to change Parent component state
     handleChange = e => {
-        let updateParent = this.props.updateParent;
         e.preventDefault();
-        
-        updateParent(this.myRef.current.value);
     }
+
+    // runs corresponding sorting algorithm from App.js
+    algoCheck = () => {
+        switch (this.myRef.current.value) {
+            case "1":
+                this.props.bubbleSort();
+                break;
+                
+            default:
+                break;
+        }
+    }
+
 
     render() {
         
         return(
             
-            <div className = "bg-primary navbar"> 
-                <form onSubmit = {this.handleChange}>
-                    <select ref = {this.myRef} className = "custom-select">
+            <div className = "bg-primary navbar topBar"> 
+                <h3>Sorting Algorithms Visualizer</h3>
+                <form className = "d-flex justify-content-center" onSubmit = {this.handleChange}>
+                    <select ref = {this.myRef} className = "custom-select mySelect">
                         <option value = "0">Choose an Algorithm</option>
                         <option value = "1">Bubble Sort</option>
                         <option value = "2">Merge Sort</option>
                         <option value = "3">Quick Sort</option>
                     </select>
-                    <Button type="submit" className = "btn-success">Visualize!</Button>
+                    <Button onClick = {this.algoCheck} type="submit" className = "btn-success myButton" id="visBtn">Visualize!</Button>
                 </form>
+                <Button onClick = {this.props.stopSort} className = "btn-secondary myButton" id = "stopSortBtn">Stop Sorting</Button>
+                <Button onClick = {this.props.regen} className = "btn-secondary myButton">Regenerate Array</Button>
             </div>
             
         );
